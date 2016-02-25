047c94e261d136fb9d6a50ddb1585ad46944c504
Samuel Ramos Barroso <samuelrb1@gmail.com>
2016-02-25 11:13:40 +0000

    La Regexp ya funciona con cualquier prefijo

--- temperature.js
+++ temperature.js
@@ -2,7 +2,7 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
+  var regexp = /^([-+]?\d+(?:\.\d*)?)\s*(?:e([-+]?\d+))?(f|fa|fah|fahrenheit|c|cel|celsius)\s*$/i;
   
   var m = temp.match(regexp);
   
